961b2869171e987cf54f633a4fd9992398492987
Jorgect99 <jorgect99@gmail.com>
2021-04-01 14:17:10 -0700

    App Core y template

--- nutrisalud/core/urls.py
+++ nutrisalud/core/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('clientes', views.clientes, name="clientes"),
+    path('formula/imc', views.imc, name="imc"),
+    path('formula/met', views.met, name="met"),
 ]
--- nutrisalud/core/views.py
+++ nutrisalud/core/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Inicio")
+    return render(request, 'core/index-dashboard.html')
+
+def clientes(request):
+    return render(request, 'core/clientes.html')
+
+def imc(request):
+    return render(request, 'core/formulas/imc.html')
+    
+def met(request):
+    return render(request, 'core/formulas/met.html')
+
